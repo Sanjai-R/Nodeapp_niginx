7da47db7e2dd99bb920662fa1a25017992532412
Sanjai-R <samsanjai777@gmail.com>
2023-04-25 07:40:21 +0530

    express server added

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(
+    "Hello World! I am being served from a Node.js app running on Nginx."
+  );
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
